d15393de9fbc03d309f3661d4a964a52d7344636
shashankd48 <jfriday464@gmail.com>
2020-05-05 00:13:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import "./App.css";
+export default function App() {
+   const [product, setProduct] = useState({
+      name: "React from facebook",
+      price: 20,
+      productBy: "Facebook",
+   });
+   const makePayment = (token) => {
+      const body = {
+         token,
+         product,
+      };
+      const header = {
+         "Content-Type": "application/json",
+      };
+      return fetch(`http://localhost:5000/payment`, {
+         method: "POST",
+         headers: header,
+         body: JSON.stringify(body),
+      })
+         .then((response) => {
+            console.log(response);
+            const { status } = response;
+            console.log("STATUS: ", status);
+         })
+         .catch((err) => console.log(err));
+   };
+   return (
+      <div className="container">
+         <div className="col 4">
+            <div className="card blue-grey darken-1">
+               <h3 className="white-text center-align">Stripe Payment</h3>
+               <StripeCheckout
+                  stripeKey={process.env.REACT_APP_KEY}
+                  toke={makePayment}
+                  name="Buy React"
+                  amount={product.price * 100}
+                  shippingAddress
+                  billingAddress
+               >
+                  <button className="btn-large pink">
+                     Buy react in just {product.price}$
+                  </button>
+               </StripeCheckout>
+            </div>
+         </div>
+      </div>
+   );
 }
-
-export default App;
